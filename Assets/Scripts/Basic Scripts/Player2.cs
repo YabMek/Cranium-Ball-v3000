f362957c545cb19eac51ee22769333e08ca9a6be
adam yonas <adamyonaswon@gmail.com>
2020-03-13 13:46:06 -0400

    Revert "Update SampleScene.unity"
    This reverts commit 4358d35eb6a09b5f511f2b0fddc91cd08986e02e.

--- Assets/Scripts/Basic Scripts/Player2.cs
+++ Assets/Scripts/Basic Scripts/Player2.cs
@@ -8,7 +8,7 @@ public class Player2 : MonoBehaviour
     private Rigidbody2D rigidBody;
     public static GameObject character;
 
-    public bool isGrounded = false;
+    public bool isGrounded;
     public Vector2 jump;
     public float jumpForce = 2.0f;
 
@@ -17,6 +17,9 @@ public class Player2 : MonoBehaviour
     private bool shouldJump;
     private bool canJump;
 
+    private bool pressedJ = false;
+
+    public float MagicTimer = 0.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -28,22 +31,32 @@ public class Player2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        print(MagicTimer);
+        MagicTimer = MagicTimer + Time.deltaTime;
         HandleMovement();
         RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up, 0.1f);
 
         if (Input.GetKeyDown(KeyCode.W))
         {
-    
-            if (isGrounded == true)
-            {
-                rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-                isGrounded = false;
-            }
+            rigidBody.AddForce(new Vector2(0, 30), ForceMode2D.Impulse);
+        }
+        else
+        {
+            rigidBody.AddForce(new Vector2(0, 0), ForceMode2D.Impulse);
         }
 
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            pressedJ = true;
+            print("JJJJJJJ");
+            animator.SetBool("Wack", true);
+            MagicTimer = 0.0f;
 
-
+        }
+        if (MagicTimer > .2f)
+        {
+            animator.SetBool("Wack", false);
+        }
 
     }
 
@@ -51,38 +64,23 @@ public class Player2 : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.D))
         {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                rigidBody.velocity = new Vector2(20, rigidBody.velocity.y);
-            }
-            else
-            {
-                rigidBody.velocity = new Vector2(5, rigidBody.velocity.y);
+            if(Input.GetKey(KeyCode.Space)) {
+               rigidBody.velocity = new Vector2(10, rigidBody.velocity.y);
+            } else {
+               rigidBody.velocity = new Vector2(5, rigidBody.velocity.y); 
             }
         }
         else if (Input.GetKey(KeyCode.A))
         {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                rigidBody.velocity = new Vector2(-20, rigidBody.velocity.y);
-            }
-            else
-            {
-                rigidBody.velocity = new Vector2(-5, rigidBody.velocity.y);
+            if(Input.GetKey(KeyCode.Space)) {
+               rigidBody.velocity = new Vector2(-10, rigidBody.velocity.y);
+            } else {
+               rigidBody.velocity = new Vector2(-5, rigidBody.velocity.y); 
             }
         }
         else
         {
             rigidBody.velocity = new Vector2(0, rigidBody.velocity.y);
-        }
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Ground")
-        {
-
-            isGrounded = true;
         }
     }
 
