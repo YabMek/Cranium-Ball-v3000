469ca1d17b173ffd9fbd88be9e9b87d3e4b309e0
YabMek <yab.mek@gmail.com>
2020-03-13 04:01:47 -0400

    Ball_Collision_Script + Ball_Collision w/ staff

---
+++ Assets/Scripts/Basic Scripts/Ball_Collision.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball_Collision : MonoBehaviour
+{
+
+    public float force = 25f;
+    public GameObject ball;
+    Rigidbody2D rbBall;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        ball = GameObject.FindGameObjectWithTag("Ball");
+        rbBall = ball.GetComponent<Rigidbody2D>();
+        print("STARTED");
+        this.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.tag == "Ball")
+        {
+            print("COLLLIDEED");
+            Vector2 pos = (transform.position - collision.gameObject.transform.position).normalized;
+            rbBall.AddForce(-pos * force, ForceMode2D.Impulse);
+
+
+        }
+    }
+}
